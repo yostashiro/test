9f43084ad1618912577f1dc2caf7e607171e4e63
Yoshi Tashiro <tashiro@roomsfor.hk>
2016-08-13 07:53:44 -0400

    [IMP] fix flake8 issues

--- mrp_bom_line_ref/mrp.py
+++ mrp_bom_line_ref/mrp.py
@@ -16,11 +16,10 @@
 #    You should have received a copy of the GNU Affero General Public License
 #    along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
-from openerp import models, fields, api, _
+from openerp import models, fields
 
 
 class MrpBomLine(models.Model):
     _inherit = 'mrp.bom.line'
 
     ref = fields.Char('Reference Number')
-
